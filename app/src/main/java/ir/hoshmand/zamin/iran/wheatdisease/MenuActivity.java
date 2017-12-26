969b60f0ec58a88846e1f7cb5bfad92ea1f71029
md._.te <m.emami9@yahoo.com>
2017-12-26 14:10:04 +0330

    Images Added

--- app/src/main/java/ir/hoshmand/zamin/iran/wheatdisease/MenuActivity.java
+++ app/src/main/java/ir/hoshmand/zamin/iran/wheatdisease/MenuActivity.java
@@ -1,13 +1,32 @@
 package ir.hoshmand.zamin.iran.wheatdisease;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.widget.ImageButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MenuActivity extends AppCompatActivity {
+
+    @BindView(R.id.btnBack)
+    ImageButton btnBack;
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.diseasesRecycler)
+    RecyclerView diseasesRecycler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.btnBack)
+    public void onViewClicked() {
     }
 }
